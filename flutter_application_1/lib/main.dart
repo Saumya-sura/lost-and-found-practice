38be29f25ea708992765f03265cf57f6e0dc0581
Saumya <surasaumya17@gmail.com>
2024-01-30 21:51:57 +0530

    Day1

---
+++ flutter_application_1/lib/main.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+void main()
+{
+  runApp(MyApp());
+}
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    int days =30;
+    String name ="Saumya";
+    
+    return MaterialApp(
+      home:  Material(
+        child:Center(
+        child:Container(
+      child:Text("welcome to $days of $name"),
+    ),
+        ),
+      ),
+    );
+  }
+}
+
