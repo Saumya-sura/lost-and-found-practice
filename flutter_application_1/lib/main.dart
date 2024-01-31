62cf6505f86f8d3c3798cc98f22a66ff41296508
Saumya <surasaumya17@gmail.com>
2024-01-31 21:24:43 +0530

    DAy2

--- flutter_application_1/lib/main.dart
+++ flutter_application_1/lib/main.dart
@@ -1,19 +1,37 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_application_1/home_page.dart';
+import 'package:flutter_application_1/pages/home_page.dart';
+import 'package:flutter_application_1/pages/login_page.dart';
+
 void main()
 {
-  runApp(MyApp());
+  runApp(const MyApp());
 }
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
   @override
-  Widget build(BuildContext context) {
-  
+  Widget build(BuildContext context) 
+  {
+    
+    
     
     return MaterialApp(
-      home: homepage(),
+      
+      themeMode: ThemeMode.light,
+      darkTheme: ThemeData(
+        //useMaterial3: false,
+        //  
+        brightness: Brightness.dark
+      ),
+      routes: {
+        "/":(context) => Login(),
+        "/home":(context) => homepage(),
+        
+      },
     );
+   
   }
+  
+ 
 }
 
